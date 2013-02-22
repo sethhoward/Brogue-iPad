32aae8d2a62c2af134a3e41d9e1ab9dab8d74085
Seth Howard <showard@itriagehealth.com>
2013-02-22 11:05:09 -0700

    Initial Commit

---
+++ iBrogue_iPad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iBrogue_iPad
+//
+//  Created by Seth Howard on 2/22/13.
+//  Copyright (c) 2013 Seth howard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ iBrogue_iPad/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  iBrogue_iPad
+//
+//  Created by Seth Howard on 2/22/13.
+//  Copyright (c) 2013 Seth howard. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
