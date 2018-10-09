1196df49be46e10d4b2fd26ae786c78d131d88e7
sethhoward <s3th.howard@gmail.com>
2018-10-09 13:26:47 -0600

    Tiniest bit of code cleanup. Fix for throw double tap issue when dialogue is too far to the left. Better touchend handling.

---
+++ iBrogue_iPad/PlatformCode/SKMagView.swift
@@ -0,0 +1,21 @@
+//
+//  SKMagView.swift
+//  iBrogue_iPad
+//
+//  Created by Seth Howard on 10/9/18.
+//  Copyright © 2018 Seth howard. All rights reserved.
+//
+
+import UIKit
+
+class SKMagView: SKView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
